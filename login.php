76de097c8e9f36572c110a49bc9d475a71558c93
Cesa-LibraryMTI <projectct97@gmail.com>
2024-01-23 05:53:35 +0530

    nefwk

--- login.php
+++ login.php
@@ -18,4 +18,16 @@
     <p>Don't have an account? <a href="/register.html">Register</a></p>
   </div>
 </body>
+<?php
+  include 'dbconnect.php';
+  $sql = "SELECT username,password FROM details WHERE username = '$uname' and password = '$pass'";
+  $result = $conn->query($sql);
+
+  if (($result !== false)&&($result->num_rows > 0)) {
+      print "<br><h1><font color = 'blue'><i>Welcome<i><font></h1><br>";
+  }else{
+      print "<br><h1><font color = 'blue'><i>Wrong credentials<i><font></h1><br>";
+  }
+  $conn->close();
+?>
 </html>
