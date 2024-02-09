6acef203d327fe565ce7b496f26f6e826e0bfcdf
Nabeel-X <kolpersky@gmail.com>
2024-02-09 17:05:49 +0530

    good

--- login.php
+++ login.php
@@ -1,42 +1,58 @@
 <html>
-<head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
     <script src = "js/login.js"></script>
     <link rel = "stylesheet" href = "styles/login.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <title>Login</title>
-</head>
-<body>
-<div class="container">
-    <i class="bi bi-person-circle icon mb-2 text-blue-500" style="font-size: 3em;"></i>
+  </head>
+  <body>
+    <div class = "loginform">  
+      <form name = "login.html" method = "POST">
 
-
-    <form method="POST">
-      <label>EMAIL:</label>
-      <input type="email" name="email" placeholder="Email" required>
-      <label>PASSWORD:</label>
-      <input type="password" name="password" id = "password" placeholder="Password" required>
-      <a href="/forgot-password">Forgot password?</a><br><br>
-      <input type="submit" class = "button" value = "Login" onclick="return check()">
-    </form>
-    <p>Don't have an account? <a href="/register.html">Register</a></p>
-  </div>
-</body>
+        <div class = "loginhead">
+          <h3>LOGIN</h3>
+        </div>
+        <table>
+          <tr>
+            <td><input type="text" name="uname" placeholder="username" required></td>
+          </tr>
+          <tr>
+            <td><input type="password" name="pass" placeholder="password" required></td>
+          </tr>
+          <tr>
+            <td><input type="submit" value="Log in" id = "loginbutton" onclick="return check()"></td>
+          </tr>
+          <tr>
+            <td>Dont have an account? <a href = "register.php">SIGN UP</a></td>
+          </tr>
+        </table>
 <?php
   include 'dbconnect.php';
-  if($_SERVER['REQUEST_METHOD']=='POST'){
-    
-    $uname = $_POST['email'];
-    $pass = $_POST['password'];
-    $sql = "SELECT username,password FROM details WHERE username = '$uname' and password = '$pass'";
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $uname = $_POST['uname'];
+    $pass = $_POST['pass'];
+    $sql = "select checker('$uname','$pass') as exist";
     $result = $conn->query($sql);
+    if($result != null){
+      $row = $result->fetch_assoc();
+      if($row['exist'] == 1){
+        session_start();
+        $_SESSION['name'] = $uname;
+        $_SESSION['logged'] = 1;
 
-    if (($result !== false)&&($result->num_rows == 1)) {
-      echo 'found';
-      header("Location: index.html");
-    }else{
-      print "<br><h1><font color = 'blue'><i>Wrong credentials<i><font></h1><br>";
-    }
-    $conn->close();
+        header('Location: index.php');
+      }else if($row['exist'] == 0){
+        session_start();
+        $_SESSION['name'] = $uname;
+        $_SESSION['logged'] = 0;
+        header('Location: about.html');
+      }else echo "<div class = 'wrongcredentials'><p>wrong credentials</p></div>";
+  }
   }
 ?>
+      </form>
+    </div>
+  </body>
 </html>
