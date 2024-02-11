cb126989a17782285fcf3d39c4a42030e3e76236
Cesa-LibraryMti <projectct97@gmail.com>
2024-02-11 19:20:19 +0530

    gkr

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loading Screen</title>
+    <title>CESA Library</title>
     
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.3/dist/tailwind.min.css" rel="stylesheet">
