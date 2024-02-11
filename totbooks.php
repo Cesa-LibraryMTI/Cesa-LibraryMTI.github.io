ab57b08e0d42c09bbf4a1f6fb78193f56930b334
Nabeel-X <kolpersky@gmail.com>
2024-02-12 01:16:50 +0530

    changed two things
    Now display all details of book in totalbooks page
    and add new book page now also insert no of copies
    into the copies table
    
    changed auto increment bid into two random gen bid

--- totbooks.php
+++ totbooks.php
@@ -31,13 +31,17 @@
                 <th>BOOK NAME</th>
                 <th>AUTHOR</th>
                 <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>COPIES</th>
+                <th>AVAILABLE</th>
+                
             </tr>
             </thead>
             <tbody>
                 
             <?php
                 include 'dbconnect.php';
-                $sql = "SELECT bid,bname,bauthor,bprice FROM books";
+                $sql = "SELECT * FROM viewbooks";
                 $result = $conn->query($sql);
 
                 
@@ -49,9 +53,11 @@
                         $bname = $row['bname'];
                         $bauthor = $row['bauthor'];
                         $bprice = $row['bprice'];
-
+                        $bcategory = $row['bcategory'];
+                        $bcopy = $row['copies'];
+                        $bavailable = $row['available'];
                         // Perform actions with the data (e.g., display or process)
-                        echo "<tr><td>$bid</td><td>$bname</td><td>$bauthor</td><td>$bprice</td></tr>";
+                        echo "<tr><td>$bid</td><td>$bname</td><td>$bauthor</td><td>$bprice</td><td>$bcategory</td><td>$bcopy</td><td>$bavailable</td></tr>";
                     }
                 }else{
                     print "<br><h1><font color = 'blue'><i>Wrong credentials<i><font></h1><br>";
