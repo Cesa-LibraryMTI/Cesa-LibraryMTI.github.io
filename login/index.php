53ddd820348b5ea1326b08790fe1c9467d887f31
Cesa-LibraryMTI <projectct97@gmail.com>
2024-02-16 10:41:28 +0530

    add admin and user light

--- login/index.php
+++ login/index.php
@@ -62,7 +62,7 @@
         session_start();
         $_SESSION['name'] = $uname;
         $_SESSION['logged'] = 0;
-        header('Location: ../about.html');
+        header('Location: ../users.php');
       }else echo "<div class = 'failed'><p>wrong credentials</p></div>";
   }
   }
